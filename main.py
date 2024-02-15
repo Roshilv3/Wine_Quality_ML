529303cb536675836f5145fde3c2755108f526a7
Roshilv3 <roshil.verma.3@gmail.com>
2024-02-15 05:55:39 +0530

    logger file created

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.Wine_Quality_ML.logger import logging
+
+logging.info("Wine Quality Project Started")
---
+++ src/Wine_Quality_ML/logger.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import logging
+from datetime import datetime
+
+log_str = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(),"log",log_str)
+os.makedirs(log_path, exist_ok=True)
+
+Log_file_path = os.path.join(log_path,log_str)
+
+logging.basicConfig(
+    filename = Log_file_path,
+    format = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]",
+    level= logging.INFO
+)
+
+logging.StreamHandler(sys.stdout)
